415aa9d1a0aaf2e55e697f933e37d340607dc852
asunathanr <nrobert2@my.athens.edu>
2019-02-10 08:21:56 -0600

    Added type annotation; meta info

--- OneTen.hs
+++ OneTen.hs
@@ -1,21 +1,27 @@
 module OneTen (myLast, myButLast) where
 
+-- FILE: OneTen.hs
+-- AUTHOR: Nathan Robertson
 -- Answers to problems 1 to 10, questions found here: https://wiki.haskell.org/99_questions/
 
 -- Problem 1
+myLast :: [a] -> a
 myLast [x] = x
 myLast li = myLast $ tail $ li
 
 -- Problem 2
+myButLast :: [a] -> a
 myButLast [x, last] = x
 myButLast li = myButLast $ tail li
 
 -- Problem 3
+elementAt :: (Eq t, Num t) => [a] -> t -> a
 elementAt li 1 = head li
 elementAt li n = elementAt (tail li) (n - 1)
-
 
 -- Problem 4
 myLength :: Num p => [a] -> p
 myLength [] = 0
 myLength li = (myLength (tail li)) + 1
+
+-- Problem 5
