616e1af907f0c1e22b582fdb3eacaf30612a1fe3
asunathanr <nrobert2@my.athens.edu>
2019-02-15 08:40:36 -0600

    Problem 10

--- OneTen.hs
+++ OneTen.hs
@@ -17,31 +17,41 @@ myLast [x] = x
 myLast li = myLast $ tail $ li
 
 -- Problem 2
+-- Find the last but one element of a list. 
 myButLast :: [a] -> a
 myButLast [x, last] = x
 myButLast [x] = x
 myButLast li = myButLast $ tail li
 
 -- Problem 3
+-- Find the K'th element of a list. The first element in the list is number 1. 
 elementAt :: (Eq t, Num t) => [a] -> t -> a
 elementAt li 1 = head li
 elementAt li n = elementAt (tail li) (n - 1)
 
 -- Problem 4
+-- Find the number of elements of a list. 
 myLength :: Num p => [a] -> p
 myLength [] = 0
 myLength li = (myLength (tail li)) + 1
 
 -- Problem 5
+-- Reverse a list
 myReverse :: [a] -> [a]
 myReverse [] = []
 myReverse li = (myReverse (tail li)) ++ [head li]
 
 -- Problem 6
+-- Find out whether a list is a palindrome. A palindrome can be read forward or backward; e.g. (x a m a x). 
 isPalindrome :: (Eq a) => [a] -> Bool
 isPalindrome li = li == myReverse li
 
 -- Problem 7
+{-
+Flatten a nested list structure.
+Transform a list, possibly holding lists as elements into a `flat' list by replacing each list with its elements (recursively). 
+We have to define a new data type, because lists in Haskell are homogeneous
+-}
 data NestedList a = Elem a | List [NestedList a]
 {-
 myFlatten (Elem x) = x
@@ -50,6 +60,10 @@ myFlatten (List [List xs]) = myFlatten xs
 -}
 
 -- Problem 8
+{-
+Eliminate consecutive duplicates of list elements.
+If a list contains repeated elements they should be replaced with a single copy of the element. The order of the elements should not be changed. 
+-}
 compress :: (Eq a) => [a] -> [a]
 compress [] = []
 compress [x] = [x]
@@ -62,7 +76,12 @@ compress (x:xs)
 -- Pack consecutive duplicates of list elements into sublists
 pack :: (Eq a) => [a] -> [[a]]
 pack [] = []
-pack (x:xs) = [(takeWhile (\item -> x == item) (x:xs))] ++ (pack (dropWhile (\item -> x == item) xs)) 
+pack (x:xs) = [takeWhile (\item -> x == item) (x:xs)] ++ (pack (dropWhile (\item -> x == item) xs)) 
           
 
 -- Problem 10
+{-
+Run-length encoding of a list. Use the result of problem P09 to implement the so-called run-length encoding data compression method. Consecutive duplicates of elements are encoded as lists (N E) where N is the number of duplicates of the element E
+-}
+encode [] = []
+encode xs = (zip (map (\subList -> length subList) (pack xs)) (map (\subList -> (head subList)) (pack xs)))
