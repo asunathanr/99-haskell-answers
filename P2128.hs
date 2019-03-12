9d1571f0db6105e9ddecd260b2969b214ae83ff7
asunathanr <nrobert2@my.athens.edu>
2019-03-12 08:06:35 -0500

    Problem 22

--- P2128.hs
+++ P2128.hs
@@ -1,4 +1,14 @@
 import ElevenTwenty
 
 -- Problem 21
+insertAt :: a -> [a] -> Int -> [a]
 insertAt x xs n = (take (n - 1) xs) ++ [x] ++ (drop (n - 1) xs)
+
+
+-- Problem 22
+-- Create a list containing all integers within a given range. 
+-- Note: Both begin and end are inclusive
+range begin end
+  | begin < end = [begin] ++ (range (begin + 1) end)
+  | begin > end = [begin] ++ (range (begin - 1) end)
+  | otherwise = [begin] 
